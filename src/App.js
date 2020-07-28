cbd02a7c47f82eaaa8c0878d97d5e7712ae7a7da
Marcos <marcosvictormbi85@gmail.com>
2020-07-27 22:56:23 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div style={{ background: "#141414" }}>
-    <Menu />
+      <Menu />
 
-    <BannerMain
+      <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
