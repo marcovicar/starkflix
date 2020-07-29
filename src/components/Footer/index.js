0ff837d8a5401c980613c2d6e234664dfa0fcaf4
Marcos <marcosvictormbi85@gmail.com>
2020-07-28 21:17:53 -0300

    Footer Logo StarkFlix alterado

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png';
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="/">
+      <img className="Logo" src={Logo} alt="StarkFlix Logo"/>
       </a>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
+        Prótotipo de uma NetFlix
       </p>
     </FooterBase>
   );
