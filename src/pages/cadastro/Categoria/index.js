327ed9b8b0a8db51873617ea2b7535871a1e39fc
Marcos <marcosvictormbi85@gmail.com>
2020-07-31 15:53:28 -0300

    corrigido urls local e de produção

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -31,7 +31,9 @@ function CadastroCategoria() {
 
   useEffect(() => {
     if (window.location.href.includes('localhost')) {
-      const URL = 'https://starkflix.herokuapp.com/categorias';
+      const URL = window.location.hostname.includes('localhost')
+        ? 'http://localhost/8080/categorias'
+        : 'https://starkflix.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
